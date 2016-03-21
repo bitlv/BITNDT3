3008959269d70ab839c41c0baac32eda024ef51a
lv <1162066366@qq.com>
2016-03-21 18:03:41 +0800

    新增检测参数设置activity

--- app/src/main/java/com/bit/lv/bitndt3/DeviceListFragment.java
+++ app/src/main/java/com/bit/lv/bitndt3/DeviceListFragment.java
@@ -1,12 +1,19 @@
 package com.bit.lv.bitndt3;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListFragment;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 import com.eurosonic.eurosonicut.EurosonicUT;
@@ -16,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ *
  * Created by lv on 2016/3/16.
  */
 public class DeviceListFragment extends ListFragment {
@@ -38,6 +46,43 @@ public class DeviceListFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
         mContentView=inflater.inflate(R.layout.list_devices,container,false);
         return mContentView;
+    }
+
+    public void onListItemClick(ListView parent,View v,int position,long id){
+        super.onListItemClick(parent, v, position, id);
+        String device=getListAdapter().getItem(position).toString();
+        dialog(device);
+
+    }
+
+    private void dialog(final String device){
+        DialogInterface.OnClickListener dialogOnClickListener=new DialogInterface.OnClickListener(){
+
+            private final Context cont=getActivity();
+
+            @Override
+        public void onClick(DialogInterface dialog,int which){
+                switch (which){
+                    case Dialog.BUTTON_POSITIVE:
+                        Toast.makeText(cont,"连接"+device,Toast.LENGTH_SHORT).show();
+                        break;
+                    case Dialog.BUTTON_NEGATIVE:
+                        Toast.makeText(cont,"取消"+device,Toast.LENGTH_SHORT).show();
+                        break;
+                    case Dialog.BUTTON_NEUTRAL:
+                        Toast.makeText(cont,"忽略"+device,Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setTitle("提示");
+        builder.setMessage("是否连接" + device + "?");
+        builder.setIcon(R.mipmap.prompt);
+        builder.setPositiveButton("连接", dialogOnClickListener);
+        builder.setNegativeButton("取消", dialogOnClickListener);
+        builder.setNeutralButton("忽略", dialogOnClickListener);
+        builder.create().show();
     }
 
 
--- app/src/main/java/com/bit/lv/bitndt3/MainActivity.java
+++ app/src/main/java/com/bit/lv/bitndt3/MainActivity.java
@@ -1,13 +1,26 @@
 package com.bit.lv.bitndt3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settings=(ImageButton)findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ParaSettings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
